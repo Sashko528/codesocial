c7241691570e0ee1599cf9f8f0574ccf5c7b99a9
Sashko528 <ivanovolexandr40@gmail.com>
2026-08-30 23:46:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,21 +27,24 @@ class PostData(BaseModel):
 @app.post("/post")
 def create_post(data: PostData):
     output = ""
-    if data.language == "python":
+    if data.language.lower() == "python":
         try:
             result = subprocess.run(["python3", "-c", data.code], capture_output=True, text=True, timeout=5)
             output = result.stdout if result.returncode == 0 else result.stderr
         except Exception as e:
             output = str(e)
-    elif data.language == "lua":
+    elif data.language.lower() == "lua":
         try:
             lua = LuaRuntime(unpack_returned_tuples=True)
             lua_logs = []
+            
             def lua_print(*args):
                 lua_logs.append(" ".join(map(str, args)))
             
-            g = lua.g
-            g.print = lua_print
+            # Використовуємо globals() замість .g
+            globals_env = lua.globals()
+            globals_env['print'] = lua_print
+            
             lua.execute(data.code)
             output = "\n".join(lua_logs)
         except Exception as e:
@@ -59,4 +62,3 @@ def create_post(data: PostData):
 def get_posts():
     cursor.execute("SELECT username, lang, code, output FROM posts ORDER BY id DESC")
     return cursor.fetchall()
-    
